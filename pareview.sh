4796e139b24f0e76563113bee913a91b8632ce99
Klaus Purer <klaus.purer@gmail.com>
2011-11-08 00:26:53 +0100

    Minor fix of a broken link

--- pareview.sh
+++ pareview.sh
@@ -95,7 +95,7 @@ hash phpcs 2>&-
 if [ $? = 0 ]; then
   DRUPALCS=`phpcs --standard=drupalcs --extensions=php,module,inc,install,test,profile,theme .`
   if [ $? = 1 ]; then
-    echo "<li><a href\"https://github.com/ericduran/drupalcs\">drupalcs</a> has found some code style issues (please check the <a href=\"http://drupal.org/node/318\">Drupal coding standards</a>):"
+    echo "<li><a href=\"https://github.com/ericduran/drupalcs\">drupalcs</a> has found some code style issues (please check the <a href=\"http://drupal.org/node/318\">Drupal coding standards</a>):"
     echo "<code>"
     echo "$DRUPALCS"
     echo "</code></li>"
