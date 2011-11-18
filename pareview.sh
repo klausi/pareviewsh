0d28ad6bedca5605b9eaf24fc2c3e5b75f29cf46
Klaus Purer <klaus.purer@gmail.com>
2011-11-18 17:02:42 +0100

    Removed function doc block test, this is now handled by drupalcs. Improved function prefix check for template and phptemplate names.

--- pareview.sh
+++ pareview.sh
@@ -203,19 +203,9 @@ if [ $? = 0 ]; then
     fi
   done
 fi
-# functions without doc blocks
-for FILE in $PHP_FILES; do
-  FUNCTIONS=`grep -E -B 1 "^function [[:alnum:]_]+.*\(.*\) ?\{" $FILE | grep -E -A 1 "^[[:space:]]*$"`
-  if [ $? = 0 ]; then
-    echo "<li>$FILE: all functions should have doxygen doc blocks, see http://drupal.org/node/1354#functions"
-    echo "<code>"
-    echo "$FUNCTIONS"
-    echo "</code></li>"
-  fi
-done
 # functions without module prefix
 for FILE in $PHP_FILES; do
-  FUNCTIONS=`grep -E "^function [[:alnum:]_]+.*\(.*\) \{" $FILE | grep -v -E "^function (_?$NAME|theme)"`
+  FUNCTIONS=`grep -E "^function [[:alnum:]_]+.*\(.*\) \{" $FILE | grep -v -E "^function (_?$NAME|theme|template|phptemplate)"`
   if [ $? = 0 ]; then
     echo "<li>$FILE: all functions should be prefixed with your module/theme name to avoid name clashes. See http://drupal.org/node/318#naming"
     echo "<code>"
