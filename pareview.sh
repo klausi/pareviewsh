89e3841ae59e1fb218d8b3dd7e395c7119b3a066
Klaus Purer <klaus.purer@gmail.com>
2011-11-20 13:34:27 +0100

    Issue #1340276: Added a check for the last character in a file to be \n.

--- pareview.sh
+++ pareview.sh
@@ -253,6 +253,19 @@ if [ -e $NAME.install ]; then
     fi
   fi
 fi
+# Last character in text files must be '\n'.
+for FILE in $TEXT_FILES; do
+  BAD_LINES=`tail -c 1 $FILE | od -c | grep '\\n'`
+  if [ $? -ne 0 ]; then
+    BAD_FILES=("${BAD_FILES[@]}" "$FILE")
+  fi
+done
+if [ -n "$BAD_FILES" ]; then
+  echo "<li>All text files should end in a single newline (\n). See http://drupal.org/node/318#indenting"
+  echo "<code>"
+  echo "${BAD_FILES[*]}"
+  echo "</code></li>"
+fi
 echo "</ul>"
 
 echo "<i>This automated report was generated with <a href=\"/sandbox/klausi/1320008\">PAReview.sh</a>, your friendly project application review script. Go and <a href=\"/project/issues/projectapplications?status=8\">review some other project applications</a>, so we can get back to yours sooner.</i>"
