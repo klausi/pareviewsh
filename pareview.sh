0c58f22090f9d486f6f03bc647eedd372c9949cb
Klaus Purer <klaus.purer@gmail.com>
2012-01-27 21:14:35 +0100

    Fixed phpcs invocation to account for the name change in drupalcs.

--- pareview.sh
+++ pareview.sh
@@ -187,7 +187,7 @@ if [ $CODER_ERROR = 0 ]; then
 fi
 
 # run drupalcs
-DRUPALCS=`phpcs --standard=DrupalCodingStandard --extensions=php,module,inc,install,test,profile,theme,js,css,info,txt .`
+DRUPALCS=`phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme,js,css,info,txt .`
 if [ $? = 1 ]; then
   echo "<li><a href=\"http://drupal.org/project/drupalcs\">Drupal Code Sniffer</a> has found some code style issues (please check the <a href=\"http://drupal.org/node/318\">Drupal coding standards</a>). See attachment.</li>"
 fi
