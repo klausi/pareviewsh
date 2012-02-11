1547c796aebe1928c93f741921c9c908690b6e16
Klaus Purer <klaus.purer@gmail.com>
2012-02-11 15:40:12 +0100

    Improved help message, drupalcs does not only check code style but also some semantics.

--- pareview.sh
+++ pareview.sh
@@ -198,7 +198,7 @@ fi
 # run drupalcs
 DRUPALCS=`phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme,js,css,info,txt .`
 if [ $? = 1 ]; then
-  echo "<li><a href=\"http://drupal.org/project/drupalcs\">Drupal Code Sniffer</a> has found some code style issues (please check the <a href=\"http://drupal.org/node/318\">Drupal coding standards</a>). See attachment.</li>"
+  echo "<li><a href=\"http://drupal.org/project/drupalcs\">Drupal Code Sniffer</a> has found some issues with your code (please check the <a href=\"http://drupal.org/node/318\">Drupal coding standards</a>). See attachment.</li>"
 fi
 echo "</ul>"
 
